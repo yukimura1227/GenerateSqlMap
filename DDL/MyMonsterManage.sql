d1e5274ebc8887d2305b2f7ce0652c7c6ffcf165
yukimura1227 <takamura1227@gmail.com>
2014-05-25 18:06:03 +0900

    サンプルとしては不要なddlとyamlファイルを削除

--- DDL/MyMonsterManage.sql
+++
@@ -1,8 +0,0 @@
-drop table IF EXISTS my_monster_manage;
-
--- テーブル定義
-create table my_monster_manage(
-  seq integer,
-  id  integer,
-  want_evolution_flag char(1) DEFAULT '1'
-);
--- DDL/TAG_MST.sql
+++
@@ -1,8 +0,0 @@
-drop table IF EXISTS TAG_MST;
-
--- テーブル定義
-create table TAG_MST(
-    tag_id  char(128)    NOT NULL,
-    tag_name varchar(100),
-	primary key (tag_id)
-);
